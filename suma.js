24f2090c3a5e80a3ee921c6878b2f362c686584b
javier-9876 <javiercastanocano@gmail.com>
2026-02-04 17:12:13 +0100

    suma js

--- suma.js
+++ suma.js
@@ -1,1 +1,8 @@
-// hola suma
+document.getElementById("sumar").onclick = sumar;
+function sumar() {
+    const n1 = Number(document.getElementById("num1").value);
+    const n2 = Number(document.getElementById("num2").value);
+
+    document.getElementById("resultado").textContent = "Resultado: " + (n1 + n2);
+    document.getElementById("resultadoResto").textContent = "";
+}
